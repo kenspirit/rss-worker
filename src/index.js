26d8c4496b7989727676f638df3e0bc1bfbb75b5
Ken Chen <chengusky@gmail.com>
2023-04-01 22:00:19 +0800

    Update

--- src/index.js
+++ src/index.js
@@ -7,32 +7,45 @@
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
-let Parser = require('rss-parser');
-let parser = new Parser();
+addEventListener('fetch', event => {
+	event.respondWith(handleRequest(event.request))
+})
 
-export default {
-	async fetch(request, env, ctx) {
-		const url = request.headers.get("x-rss");
-		const feed = await parser.parseURL(url);
-		const data = {
-			title: feed.title
-		}
+function getRssField(item, fieldName) {
+	const field = item.querySelector(fieldName);
+	if (field) {
+		return field.textContent;
+	}
+}
 
-		data.items = feed.items.map(item => {
-			return {
-				title: item.title,
-				creator: item.creator,
-				pubDate: item.pubDate,
-				link: item.link
-			};
-		});
+async function handleRequest(request) {
+	// Get the URL of the RSS feed from the request query parameters
+	const url = new URL(request.url)
+	const feedUrl = url.searchParams.get('url')
 
-		const json = JSON.stringify(data, null, 2);
+	// Fetch the RSS feed
+	const response = await fetch(feedUrl)
+	const text = await response.text()
 
-		return new Response(json, {
-			headers: {
-				"content-type": "application/json;charset=UTF-8",
-			},
-		});
-	},
+	// Parse the RSS feed and extract the items
+	const parser = new DOMParser()
+	const doc = parser.parseFromString(text, 'text/xml')
+	const items = doc.querySelectorAll('item')
+
+	// Create an array of objects representing the items in the RSS feed
+	const data = []
+	items.forEach(item => {
+		data.push({
+			title: getRssField(item, 'title'),
+			link: getRssField(item, 'link'),
+			pubDate: getRssField(item, 'pubDate'),
+			author: getRssField(item, 'author'),
+			description: getRssField(item, 'description')
+		})
+	})
+
+	// Return the data as JSON
+	return new Response(JSON.stringify(data), {
+		headers: { 'Content-Type': 'application/json' }
+	})
 };
