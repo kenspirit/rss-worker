c986d4f33b5237e7fc919c4ada3c95d928958d67
Ken Chen <chengusky@gmail.com>
2023-04-02 00:00:09 +0800

    Add null url handling

--- src/index.js
+++ src/index.js
@@ -7,46 +7,43 @@
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
-// addEventListener('fetch', event => {
-// 	event.respondWith(handleRequest(event.request))
-// })
 
 const htmlparser2 = require('htmlparser2');
-
-function getRssField(item, fieldName) {
-	const field = item.querySelector(fieldName);
-	if (field) {
-		return field.textContent;
-	}
-}
 
 async function handleRequest(request) {
 	// Get the URL of the RSS feed from the request query parameters
 	const url = new URL(request.url)
 	const feedUrl = url.searchParams.get('url')
+	let feed;
+	if (!feedUrl) {
+		feed = {
+			"type": "atom",
+			"items": []
+		}
+	} else {
+		// Fetch the RSS feed
+		const response = await fetch(feedUrl)
+		const text = await response.text()
 
-	// Fetch the RSS feed
-	const response = await fetch(feedUrl)
-	const text = await response.text()
+		// Parse the RSS feed and extract the items
+		// const parser = new DOMParser()
+		// const doc = parser.parseFromString(text, 'text/xml')
+		// const doc = htmlparser2.parseDocument(text);
+		// const items = doc.querySelectorAll('item')
 
-	// Parse the RSS feed and extract the items
-	// const parser = new DOMParser()
-	// const doc = parser.parseFromString(text, 'text/xml')
-	// const doc = htmlparser2.parseDocument(text);
-	// const items = doc.querySelectorAll('item')
-
-	// // Create an array of objects representing the items in the RSS feed
-	// const data = []
-	// items.forEach(item => {
-	// 	data.push({
-	// 		title: getRssField(item, 'title'),
-	// 		link: getRssField(item, 'link'),
-	// 		pubDate: getRssField(item, 'pubDate'),
-	// 		author: getRssField(item, 'author'),
-	// 		description: getRssField(item, 'description')
-	// 	})
-	// })
-	const feed = htmlparser2.parseFeed(text);
+		// // Create an array of objects representing the items in the RSS feed
+		// const data = []
+		// items.forEach(item => {
+		// 	data.push({
+		// 		title: getRssField(item, 'title'),
+		// 		link: getRssField(item, 'link'),
+		// 		pubDate: getRssField(item, 'pubDate'),
+		// 		author: getRssField(item, 'author'),
+		// 		description: getRssField(item, 'description')
+		// 	})
+		// })
+		feed = htmlparser2.parseFeed(text);
+	}
 
 	// Return the data as JSON
 	return new Response(JSON.stringify(feed), {
